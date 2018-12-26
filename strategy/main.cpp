93fafe77a2758bcd3114ec4e8373339bb2b83fea
Hanqiu Jiang <hanq.jiang@gmail.com>
2018-12-26 13:31:05 +0100

    doc: Typos

--- strategy/main.cpp
+++ strategy/main.cpp
@@ -5,26 +5,25 @@
 /** Strategy Pattern
  *
  * The main idea of the strategy pattern is to encapsulate algorithms
- * in an object. The key to appling the this pattern is designing interfaces
- * for the strategy and its content that are general enough to support
- * a range of algorithms.
+ * in an object. The key in applying this pattern is to design interfaces
+ * for the strategy that are general enough to support a range of algorithms.
  *
- * Printer does not have to know, which strategies exist, as long
- * the interface which is the Print function is defined. Adding new
- * strategies one does not have to change the Printer class
+ * Printer does not have to know, which strategies exist, as long as
+ * the interface (Print function) is defined. Adding new
+ * strategies does not require changing the Printer class
  *
  */
 
 
 int main() {
-  // Create to be printed vector
+  // Create vector we want to print
   std::vector<int> vector = {0,1,2,6,5,4,3,7,8,9};
 
   // Instantiate printer object which represents the user interface
   Printer printer;
 
 
-  // Set different strategies to print out vector
+  // Set different strategies to print vector
   printer.SetStrategy(new InOrderPrint);
   printer.Print(vector);
 
